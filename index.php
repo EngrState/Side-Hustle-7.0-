a11e20ac1f64f49a816b5b904db57ab9dd58307a
EngrState <103576686+EngrState@users.noreply.github.com>
2023-03-09 21:15:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
+<?php
+//variable 
+$learn = "constant learning can will improve durability and perfection by ";
+echo $learn;
+// defining a constant;
 
+define("Name", "ENGR STATE");
+echo Name;
+?>
